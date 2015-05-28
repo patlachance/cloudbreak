f8916310b25fdb1fa0d43c5d5647c2cfd4e9bd23
Tamas Bihari <tamas.bihari@sequenceiq.com>
2015-05-28 18:00:18 +0200

    CLOUD-774 validate igw and vpc ids before starting stack creation

--- core/src/main/java/com/sequenceiq/cloudbreak/service/stack/connector/aws/AwsProvisionSetup.java
+++ core/src/main/java/com/sequenceiq/cloudbreak/service/stack/connector/aws/AwsProvisionSetup.java
@@ -3,8 +3,16 @@ package com.sequenceiq.cloudbreak.service.stack.connector.aws;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeInternetGatewaysRequest;
+import com.amazonaws.services.ec2.model.DescribeInternetGatewaysResult;
+import com.amazonaws.services.ec2.model.InternetGateway;
+import com.amazonaws.services.ec2.model.InternetGatewayAttachment;
+import com.sequenceiq.cloudbreak.domain.AwsNetwork;
 import com.sequenceiq.cloudbreak.domain.CloudPlatform;
 import com.sequenceiq.cloudbreak.domain.Stack;
 import com.sequenceiq.cloudbreak.service.stack.connector.ProvisionSetup;
@@ -14,6 +22,9 @@ import com.sequenceiq.cloudbreak.service.stack.event.ProvisionSetupComplete;
 @Component
 public class AwsProvisionSetup implements ProvisionSetup {
 
+    @Inject
+    private AwsStackUtil awsStackUtil;
+
     @Override
     public ProvisionEvent setupProvisioning(Stack stack) {
         return new ProvisionSetupComplete(getCloudPlatform(), stack.getId())
@@ -22,7 +33,24 @@ public class AwsProvisionSetup implements ProvisionSetup {
 
     @Override
     public String preProvisionCheck(Stack stack) {
-        return null;
+        String result = null;
+        AwsNetwork network = (AwsNetwork) stack.getNetwork();
+        if (network.isExistingVPC()) {
+            AmazonEC2Client amazonEC2Client = awsStackUtil.createEC2Client(stack);
+            DescribeInternetGatewaysRequest describeInternetGatewaysRequest = new DescribeInternetGatewaysRequest();
+            describeInternetGatewaysRequest.withInternetGatewayIds(network.getInternetGatewayId());
+            DescribeInternetGatewaysResult describeInternetGatewaysResult = amazonEC2Client.describeInternetGateways(describeInternetGatewaysRequest);
+            if (describeInternetGatewaysResult.getInternetGateways().size() < 1) {
+                result = "The given internet gateway does not exist or belongs to a different region on AWS.";
+            } else {
+                InternetGateway internetGateway = describeInternetGatewaysResult.getInternetGateways().get(0);
+                InternetGatewayAttachment attachment = internetGateway.getAttachments().get(0);
+                if (attachment != null && !attachment.getVpcId().equals(network.getVpcId())) {
+                    result = "The given internet gateway does not belong to the given VPC.";
+                }
+            }
+        }
+        return result;
     }
 
     public Map<String, Object> getSetupProperties(Stack stack) {
